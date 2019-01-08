45cdcb00c979214b697643f5c704db266aea015b
Tom Wells <tom@synthesis.co.za>
2019-01-08 17:05:30 +0200

    removed the client from the operator

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,4 +1,4 @@
 vendor/k8s.io/code-generator/generate-groups.sh all \
-github.com/synthesis-labs/polaris-operator/pkg/client \
+github.com/synthesis-labs/polaris-client/pkg/client \
 github.com/synthesis-labs/polaris-operator/pkg/apis \
 polaris:v1alpha1
